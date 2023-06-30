97c73706f22c6120d36a29411d48976f352b1905
deiay <davidaideyan@gmail.com>
2023-06-29 22:15:10 -0400

    add initial django server

---
+++ users/views.py
@@ -0,0 +1,22 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+
+
+class UserCreateAPIView(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        if not username or not password:
+            return Response(
+                {"error": "Username and password required"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        user = User(username=username, password=make_password(password))
+        user.save()
+
+        return Response({"message": "User created"}, status=status.HTTP_201_CREATED)
